123426558088d17755e9e82601adb6e662a154d3
Aditya Kumar <kaditya41057@gmail.com>
2025-11-07 21:44:56 +0530

    doWhile

---
+++ basic/loops/doWhile.java
@@ -0,0 +1,12 @@
+package basic.loops;
+
+public class doWhile {
+    public static void main(String[] args) {
+        int counter = 1;
+        do{
+            System.out.println("Aditya kumar Chaudhary");
+            counter++;
+        }while(counter<=10);
+    }
+    
+}
