5d59d655d0e636e56c0615d644b35034a67c5911
Aditya Kumar <kaditya41057@gmail.com>
2025-11-07 12:33:24 +0530

    leap_year

---
+++ basic/conditional_question/leap_year.java
@@ -0,0 +1,17 @@
+package basic.conditional_question;
+import java.util.*;
+
+public class leap_year {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter year:");
+        int year = sc.nextInt();
+        if((year%4 ==0 ) && (year % 100 != 0 || year%400==0)){
+            System.out.println("LY");
+
+        }else{
+            System.out.println("NLY");
+        }
+    }
+    
+}
