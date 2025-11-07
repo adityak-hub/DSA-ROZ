38cd9f8fc1bba4abbf74982d80a43504d3840f34
Aditya Kumar <kaditya41057@gmail.com>
2025-11-07 23:35:31 +0530

    Factorial

---
+++ basic/loops/Factorial.java
@@ -0,0 +1,16 @@
+package basic.loops;
+import java.util.*;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your number: ");
+        int n = sc.nextInt();
+        int fact=1; 
+        for(int i=1; i<=n; i++){
+            fact *= i;
+        }
+        System.out.println("The factorail of given number is = " + fact);
+        
+    }
+}
